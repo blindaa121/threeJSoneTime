a032a6d7d88cabdb316ac37e41a439a72807716b
Bryan <blindaa121@gmail.com>
2020-07-12 20:14:40 -0700

    Add textures

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    renderer = new THREE.WebGLRenderer({ antialias: true });
 
-const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(renderer.domElement);
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    const texture = new THREE.TextureLoader().load('./textures/water.jpg');
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-camera.position.z = 5;
+    camera.position.z = 5;
+}
 
 function animate() {
     requestAnimationFrame(animate);
 
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
     renderer.render(scene, camera); // takes in scene and camera;
 }
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
